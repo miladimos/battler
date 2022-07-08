394ecf0adc878f730011f4bb06954082d9017fb2
miladimos <milad.jafari6210@gmail.com>
2022-07-08 21:01:33 +0430

    edit player script

--- src/game/scripts/player/player.gd
+++ src/game/scripts/player/player.gd
@@ -1,45 +1,56 @@
 extends KinematicBody
 
-var speed = 10
-var mouse_sensitivity = 0.03
-var direction = Vector3()
+var volecity = Vector3()
+var camera_x_rotation = 0
+
+export var speed = 20
+export var acceleration = 5
+export var jump_power = 30
+export var gravity = 0.98
+export var mouse_sensitivity = 0.1
 
 onready var head = $head
+onready var camera = $head/camera
 
 func _ready():
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
-		head.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
-		head.rotation.x = clamp(head.rotation.x, deg2rad(-80), deg2rad(80))
 		
-func _process(delta):
-	pass
+		var x_delta = event.relative.y * mouse_sensitivity
+		if camera_x_rotation + x_delta > -90 and camera_x_rotation + x_delta < 90:
+			camera.rotate_x(deg2rad(-x_delta))
+			camera_x_rotation += x_delta	
+			
+
 	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
 func _physics_process(delta):
-	direction = Vector3()
+	var direction = Vector3()
+	var head_basis = head.get_global_transform().basis
 	
 	if Input.is_action_pressed("player_move_forward"):
-		direction -= transform.basis.z
-	if Input.is_action_pressed("player_move_backward"):
-		direction += transform.basis.z
-	if Input.is_action_pressed("player_move_left"):
-		direction -= transform.basis.x
-	if Input.is_action_pressed("player_move_right"):
-		direction += transform.basis.x
+		direction -= head_basis.z
+	elif Input.is_action_pressed("player_move_backward"):
+		direction += head_basis.z
+	elif Input.is_action_pressed("player_move_right"):
+		direction += head_basis.x
+	elif Input.is_action_pressed("player_move_left"):
+		direction -= head_basis.x
 	
 	direction = direction.normalized()
 	
-	move_and_slide(direction * speed, Vector3.UP)
+	volecity = volecity.linear_interpolate(direction * speed, acceleration * delta)
+	volecity.y -= gravity
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	if Input.is_action_just_released("player_jump") and is_on_floor():
+		volecity.y += jump_power
+		
+	volecity = move_and_slide(volecity, Vector3.UP)		
+		
+
